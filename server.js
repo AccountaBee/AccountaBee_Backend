21bd99a3e9d8346c49b29f4e04870ecfa6876786
Valerie Ingmann <valerieingmann707@gmail.com>
2020-10-08 17:21:08 -0400

    starting point

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+	res.send("TESTING GET");
+});
+app.get("/api", (req, res, next) => {
+	res.send("TESTING API");
+});
+
+const port = 8080;
+app.listen(port, () => {
+	console.log("Running on port 8080");
+});
