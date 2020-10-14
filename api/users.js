d1484ad6a868c843a890b2522f488b712feaaa14
Valerie Ingmann <valerieingmann707@gmail.com>
2020-10-14 14:48:09 -0400

    chore: testing user routes

--- api/users.js
+++ api/users.js
@@ -3,15 +3,33 @@ const { Goal, User } = require("../db/models");
 
 // signup route, expecting firebase user id and firstName in req.body
 router.post("/signup", async (req, res, next) => {
-	const { uid, firstName } = req.body;
+	const { uid, firstName, email, password } = req.body;
 	const [user] = await User.findOrCreate({
 		where: {
-			fbId: uid,
-			firstName
+			uid,
+			firstName,
+			email,
+			password
 		}
 	});
 	console.log(user);
 	res.json(user);
+});
+
+router.post("/login", async (req, res, next) => {
+	try {
+		const { uid, email, password } = req.body;
+		const user = await User.findByPk(uid, { include: Goal });
+		if (!user.correctPassword(password)) {
+			console.log("Incorrect password for user:", email);
+			res.status(401).send("Wrong username and/or password");
+		} else {
+			console.log(user);
+			res.json(user);
+		}
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
--- db/models/User.js
+++ db/models/User.js
@@ -1,4 +1,4 @@
-// const crypto = require("crypto");
+const crypto = require("crypto");
 const Sequelize = require("sequelize");
 const db = require("../db");
 
@@ -8,62 +8,63 @@ const User = db.define("user", {
 		allowNull: false
 	},
 	uid: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		primaryKey: true
+	},
+	email: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false,
+		validate: {
+			isEmail: true
+		}
+	},
+	password: {
+		type: Sequelize.STRING,
+		get() {
+			return () => this.getDataValue("password");
+		}
+	},
+	salt: {
+		type: Sequelize.STRING,
+		get() {
+			return () => this.getDataValue("salt");
+		}
 	}
-	// email: {
-	// 	type: Sequelize.STRING,
-	// 	unique: true,
-	// 	allowNull: false,
-	// 	validate: {
-	// 		isEmail: true
-	// 	}
-	// },
-	// password: {
-	// 	type: Sequelize.STRING,
-	// 	get() {
-	// 		return () => this.getDataValue("password");
-	// 	}
-	// },
-	// salt: {
-	// 	type: Sequelize.STRING,
-	// 	get() {
-	// 		return () => this.getDataValue("salt");
-	// 	}
-	// }
 });
 
 module.exports = User;
 
-// /**
-//  * instanceMethods
-//  */
-// User.prototype.correctPassword = function (candidatePwd) {
-// 	return User.encryptPassword(candidatePwd, this.salt()) === this.password();
-// };
+/**
+ * instanceMethods
+ */
+User.prototype.correctPassword = function (candidatePwd) {
+	return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+};
 
-// /**
-//  * classMethods
-//  */
-// User.generateSalt = function () {
-// 	return crypto.randomBytes(16).toString("base64");
-// };
+/**
+ * classMethods
+ */
+User.generateSalt = function () {
+	return crypto.randomBytes(16).toString("base64");
+};
 
-// User.encryptPassword = function (plainText, salt) {
-// 	return crypto.createHash("RSA-SHA256").update(plainText).update(salt).digest("hex");
-// };
+User.encryptPassword = function (plainText, salt) {
+	return crypto.createHash("RSA-SHA256").update(plainText).update(salt).digest("hex");
+};
 
-// /**
-//  * hooks
-//  */
-// const setSaltAndPassword = user => {
-// 	if (user.changed("password")) {
-// 		user.salt = User.generateSalt();
-// 		user.password = User.encryptPassword(user.password(), user.salt());
-// 	}
-// };
+/**
+ * hooks
+ */
+const setSaltAndPassword = user => {
+	if (user.changed("password")) {
+		user.salt = User.generateSalt();
+		user.password = User.encryptPassword(user.password(), user.salt());
+	}
+};
 
-// User.beforeCreate(setSaltAndPassword);
-// User.beforeUpdate(setSaltAndPassword);
-// User.beforeBulkCreate(users => {
-// 	users.forEach(setSaltAndPassword);
-// });
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
+User.beforeBulkCreate(users => {
+	users.forEach(setSaltAndPassword);
+});
