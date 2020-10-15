77fd152a240956b1b1fbf77a18547dacc3657be0
Valerie Ingmann <valerieingmann707@gmail.com>
2020-10-15 16:20:55 -0400

    chore: changed file name for  google config

--- gcpSetup.js
+++ gcpSetup.js
@@ -1,2 +1,2 @@
 const fs = require("fs");
-fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, err => {});
+fs.writeFile("./google-credentials-heroku.json", process.env.GCP_CRED, err => {});
