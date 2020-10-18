66b4ee5625a30d849014bbfbc36a4c69f08b39fc
Valerie Ingmann <valerieingmann707@gmail.com>
2020-10-18 10:06:32 -0400

    chore: rm commented out express session code

--- server.js
+++ server.js
@@ -1,55 +1,22 @@
-const express = require('express');
-const db = require('./db/db');
+const express = require("express");
+const db = require("./db/db");
 const app = express();
-
-// const session = require("express-session");
-// const passport = require("passport");
-// const SequelizeStore = require("connect-session-sequelize")(session.Store);
-// const sessionStore = new SequelizeStore({ db });
-
-// sessionStore.sync();
-
-// // passport registration
-// passport.serializeUser((user, done) => done(null, user.id));
-
-// passport.deserializeUser(async (id, done) => {
-// 	try {
-// 		const user = await db.models.user.findByPk(id);
-// 		done(null, user);
-// 	} catch (err) {
-// 		done(err);
-// 	}
-// });
 
 // body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// session middleware with passport
-// app.use(
-// 	session({
-// 		secret: process.env.SESSION_SECRET || "my best friend is Cody",
-// 		store: sessionStore,
-// 		resave: false,
-// 		saveUninitialized: false
-// 	})
-// );
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// app.use("/auth", require("./auth"));
-app.use('/api', require('./api'));
+app.use("/api", require("./api"));
 
 app.use((err, req, res, next) => {
 	console.error(err);
 	console.error(err.stack);
-	res.status(err.status || 500).send(err.message || 'Internal server error.');
+	res.status(err.status || 500).send(err.message || "Internal server error.");
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-	console.log('Running on port 8080');
+	console.log("Running on port 8080");
 });
 
 db.sync();
