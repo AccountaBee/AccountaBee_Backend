921e802675e06abd22ef7407e3684ea7ddc04a0d
Alexandra Langton <alexandra.langton@outlook.com>
2020-10-22 12:30:48 -0400

    Test: backend routes for picture

--- api/users.js
+++ api/users.js
@@ -1,57 +1,76 @@
-const router = require("express").Router();
-const { Goal, User } = require("../db/models");
-const admin = require("../firebase.config");
+const router = require('express').Router();
+const { Goal, User } = require('../db/models');
+const admin = require('../firebase.config');
 
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
 	try {
 		const { token } = req.body;
 		const decodedToken = await admin.auth().verifyIdToken(token);
 		const uid = decodedToken.uid;
-		console.log("decoded token: ", uid);
+		console.log('decoded token: ', uid);
 	} catch (error) {
 		next(error);
 	}
 });
 
 // signup route, expecting token, firstName, and email in req.body
-router.post("/signup", async (req, res, next) => {
+router.post('/signup', async (req, res, next) => {
 	try {
 		const { token, firstName, email } = req.body;
-		console.log("req.body: ", req.body);
+		console.log('req.body: ', req.body);
 		const decodedToken = await admin.auth().verifyIdToken(token);
 		const uid = decodedToken.uid;
-		console.log("decoded token: ", uid);
+		console.log('decoded token: ', uid);
 		const [user] = await User.findOrCreate({
 			where: {
 				uid,
 				firstName,
-				email
-			}
+				email,
+			},
 		});
-		console.log("user after creation: ", user);
+		console.log('user after creation: ', user);
 		res.json(user);
 	} catch (error) {
-		console.log("error!! ", error);
+		console.log('error!! ', error);
 	}
 });
 
 // expecting token in req.body
 
-router.post("/login", async (req, res, next) => {
+router.post('/login', async (req, res, next) => {
 	try {
 		const { token } = req.body;
 		const decodedToken = await admin.auth().verifyIdToken(token);
-		console.log("DECODED TOKEN", decodedToken);
+		console.log('DECODED TOKEN', decodedToken);
 		const uid = decodedToken.uid;
-		console.log("UID", uid);
+		console.log('UID', uid);
 		const user = await User.findOne({
 			where: {
-				uid
+				uid,
 			},
-			include: {
-				model: Goal
-			}
 		});
+		res.json(user);
+	} catch (error) {
+		next(error);
+	}
+});
+
+// route to add a profile picture
+router.patch('/picture', async (req, res, next) => {
+	try {
+		console.log('REQ.BODY --------> ', req.body);
+		const { token } = req.body;
+		const decodedToken = await admin.auth().verifyIdToken(token);
+		console.log('DECODED TOKEN', decodedToken);
+		const uid = decodedToken.uid;
+		console.log('UID', uid);
+		const user = await User.findOne({
+			where: {
+				uid,
+			},
+		});
+		// user.profilePicture = req.body.photo
+		// await user.save();
 		res.json(user);
 	} catch (error) {
 		next(error);
--- db/models/User.js
+++ db/models/User.js
@@ -1,23 +1,26 @@
-const Sequelize = require("sequelize");
-const db = require("../db");
+const Sequelize = require('sequelize');
+const db = require('../db');
 
-const User = db.define("user", {
+const User = db.define('user', {
 	firstName: {
 		type: Sequelize.STRING,
-		allowNull: false
+		allowNull: false,
 	},
 	uid: {
 		type: Sequelize.STRING,
-		primaryKey: true
+		primaryKey: true,
 	},
 	email: {
 		type: Sequelize.STRING,
 		unique: true,
 		allowNull: false,
 		validate: {
-			isEmail: true
-		}
-	}
+			isEmail: true,
+		},
+	},
+	profilePicture: {
+		type: Sequelize.BLOB('long'),
+	},
 });
 
 module.exports = User;
